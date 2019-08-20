b665ffcfe8e57f804c8781a353fe2222bb01e947
Moshim Yabiku Junior <gojr@gft.com>
2019-08-20 09:40:17 -0300

    Estudo Deploy

---
+++ EstudoDeploy/Controllers/ConversorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EstudoDeploy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConversorController : ControllerBase
+    {
+        [HttpGet("{libras}")]
+        public object Get(double libras)
+        {
+            return new
+            {
+                Libras = libras,
+                Kilos = libras * 0.453592
+            };
+        }
+    }
+}
